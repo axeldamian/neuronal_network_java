329145cffcbfb31616bbef865bae5c20a1fcd3de
axeldamian <axel.arrondo@mercadolibre.com>
2024-12-07 21:32:37 -0300

    log at startup when the application starts

--- leibnix/src/main/java/com/leibnix/leibnix/LeibnixApplication.java
+++ leibnix/src/main/java/com/leibnix/leibnix/LeibnixApplication.java
@@ -1,13 +1,18 @@
 package com.leibnix.leibnix;
 
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class LeibnixApplication {
 
+	private static final Logger log = LogManager.getLogger(LeibnixApplication.class);
+
 	public static void main(String[] args) {
 		SpringApplication.run(LeibnixApplication.class, args);
+		log.info("application initialized");
 	}
 
 }
